8f9f2905e1c44feb365f3529bcc58a1c71f2434b
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-05-09 15:26:16 -0400

    exporting files

---
+++ javaScript/play.js
@@ -0,0 +1,19 @@
+var name = "omar";
+var age = "37";
+var hasHobbies = "true";
+
+function summarizeUser(userName, userAge, userHobbies) {
+  return (
+    "Name is " +
+    userName +
+    ", Age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHobbies
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
+
+
+
---
+++ notes-app/app.js
@@ -0,0 +1,7 @@
+// const add = require('./utils.js');
+// const sum = add(4, -2);
+// console.log(sum);
+
+const getNotes = require('./notes.js');
+
+console.log(getNotes());
