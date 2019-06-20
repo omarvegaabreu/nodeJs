e51706690c493eb8a53be0198c022d0779b9e7e5
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-20 11:53:31 -0400

    server request using raw node

--- server/routes.js
+++ server/routes.js
@@ -22,7 +22,7 @@ const requestHandler = (req, res) => {
     });
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
+      const message = parsedBody.split("=")[0];
       fs.writeFile("message.txt", message, err => {
         fs.statusCode = 302;
         res.setHeader("Location", "/");
