27beca09bcadadb3da363e4e6c336cb76c6c74be
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-21 22:23:49 -0400

    practicing routes

--- assigment_3/routes/index.js
+++ assigment_3/routes/index.js
@@ -1,13 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Route 1");
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
 
-router.get("/user", (req, res, next) => {
-  res.send("Route 2");
+router.get("/users", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "views", "users.html"));
 });
 
 module.exports = router;
