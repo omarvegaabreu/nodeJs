cacf17baa9aff742de04223c075fb7dffa3526c5
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-27 13:41:17 -0400

    finished with pug

--- assigment_4/app.js
+++ assigment_4/app.js
@@ -1,22 +1,29 @@
+//NPM dependencies
 const express = require("express");
+const bodyParser = require("body-parser");
 
 //created app
 const app = express();
 
+const users = [];
+
 //templating engine
 app.set("view engine", "pug");
 app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res, next) => {
   res.render("index", { pageTitle: "Add user" });
 });
 
 app.get("/users", (req, res, next) => {
-  res.render("users", { pageTitle: "User" });
+  res.render("users", { pageTitle: "User", users: users });
 });
 
 app.post("/add-user", (req, res, next) => {
-  res.redirect("/ rs");
+  users.push({ name: req.body.username });
+  res.redirect("/users");
 });
 
 //server
