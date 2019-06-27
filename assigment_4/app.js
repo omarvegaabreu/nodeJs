01fc88efb9423899d3271f7c090ba0b30cd5870d
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-27 14:31:29 -0400

    added ejs files

--- assigment_4/app.js
+++ assigment_4/app.js
@@ -10,7 +10,7 @@ const users = [];
 
 //templating engine
 app.engine("hbs", expressHbs({ defaultLayout: "main-layout", extname: "hbs" }));
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
