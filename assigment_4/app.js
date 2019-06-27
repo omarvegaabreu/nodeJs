98c4b9b9f9a26cd28234340d039175049cb45d6d
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-27 12:56:50 -0400

    added get and post requests

--- assigment_4/app.js
+++ assigment_4/app.js
@@ -3,18 +3,21 @@ const express = require("express");
 //created app
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.render();
+//templating engine
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+  res.render("index", { pageTitle: "Add users" });
 });
 
-app.get('users', (req, res, next) {
-  res.render();
+app.get("/users", (req, res, next) => {
+  res.render("user", { pageTitle: "User" });
 });
 
-app.post('/add-user', (req, res, next) => {
-  res.redirect('/users');
+app.post("/add-user", (req, res, next) => {
+  res.redirect("/users");
 });
-
 
 //server
 app.listen(3000);
